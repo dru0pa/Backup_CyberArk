ff9c1e33f1213456ae54baa2854125f9936a1346
dru0pa <dru0pa@gmail.com>
2023-01-19 14:36:43 +0200

    Update PAReplicate.py

--- PAReplicate.py
+++ PAReplicate.py
@@ -1,19 +1,24 @@
-#Welcome to our script that automates the execution of CyberArk PAReplicate.exe and emails the log file PAReplicate.log, 
-#which has been zipped and password protected.
-#This script was developed with the help of OpenAI.
-##This script is designed to make the process of running PAReplicate.exe and managing the log files as efficient and secure as possible.
-#The script will execute the PAReplicate.exe and create a log file named PAReplicate.log. 
-#This log file will be zipped and password protected for added security. 
-#Once the script has completed, an email will be sent to the specified recipient with the zipped, password-protected log file as an attachment.
+# Welcome to our script that automates the execution of CyberArk PAReplicate.exe and emails the log file PAReplicate.log, 
+# which has been zipped and password protected.
+# This script was developed with the help of OpenAI.
 #
-#Before running the script, please ensure that you have the necessary dependencies installed, such as Python and the required libraries. 
-#You will also need to have access to the PAReplicate.exe file, the email credentials and the email address of the recipient to whom the log file will be sent.
+# This script is designed to make the process of running PAReplicate.exe and managing the log files as efficient and secure as possible.
+# The script will execute the PAReplicate.exe and create a log file named PAReplicate.log. 
+# This log file will be zipped and password protected for added security. 
+# Once the script has completed, an email will be sent to the specified recipient with the zipped, password-protected log file as an attachment.
 #
-#The script is well commented and easy to understand, so it can be easily modified to suit your specific needs.
+# Before running the script, please ensure that you have the necessary dependencies installed, such as Python and the required libraries. 
+# You will also need to have access to the PAReplicate.exe file, the email credentials and the email address of the recipient to whom the log file will be sent.
 #
-#To run the script, simply navigate to the script directory in the command prompt and run the command: python PAReplicate.py
+# The script is well commented and easy to understand, so it can be easily modified to suit your specific needs.
 #
-#Thank you for using our script, and please feel free to reach out to us if you have any questions or need any assistance.
+# Update the pathe to the file location from C drive to the appropriate location
+#
+# Update the SMTP variable accordingly as well as uncomment the #server.starttls()
+#
+# To run the script, simply navigate to the script directory in the command prompt and run the command: python PAReplicate.py
+#
+# Thank you for using our script, and please feel free to reach out to us if you have any questions or need any assistance.
 #
 import subprocess
 import os
